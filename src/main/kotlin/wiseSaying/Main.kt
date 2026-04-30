320b054a74640080f2aeb93cc1432e514c5580ce
choiyunseo <choeyunseo4060@naver.com>
2026-04-30 11:17:22 +0900

    3강 - 등록 추가 기능

--- src/main/kotlin/wiseSaying/Main.kt
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -1,6 +1,9 @@
 package wiseSaying
 
 fun main() {
+
+    var lastId = 0
+
     println("== 명언 앱 ==")
 
     while(true) {
@@ -18,6 +21,9 @@ fun main() {
 
                 print("작가: ")
                 val author = readln()
+
+                val id = ++lastId
+                println("${id}번 명언이 등록되었습니다.")
             }
         }
 
