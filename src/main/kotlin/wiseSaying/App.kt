cafe02f2be2d5d78aaf57a05912d8f5624e2b75a
choiyunseo <choeyunseo4060@naver.com>
2026-04-30 14:24:08 +0900

    10강 - SystemController, WiseSayingController 도입

--- src/main/kotlin/wiseSaying/App.kt
+++ src/main/kotlin/wiseSaying/App.kt
@@ -1,9 +1,12 @@
 package wiseSaying
 
-class App {
+class App(
+    val wiseSayingController: WiseSayingController = WiseSayingController(),
+    val systemController: SystemController = SystemController()
+) {
 
     fun run() {
-        var lastId = 0 // 우선 val로 하고, 수정이 필요한 변수이면 var로 바꿔준다.
+        var lastId = 0
         val wiseSayings = mutableListOf<WiseSaying>()
 
         println("== 명언 앱 ==")
@@ -14,66 +17,12 @@ class App {
             val input = readln()
             val rq = Rq(input)
 
-            when (input) {
-                "종료" -> {
-                    println("프로그램을 종료합니다.")
-                    break
-                }
-
-                "등록" -> {
-                    print("명언: ")
-                    val content = readln()
-
-                    print("작가: ")
-                    val author = readln()
-
-                    val id = ++lastId
-
-                    WiseSaying(id, content, author)
-                        .also { wiseSayings.add(it) }
-
-                    println("${id}번 명언이 등록되었습니다.")
-                }
-
-                "목록" -> {
-                    println("번호 / 작가 / 명언")
-                    println("-".repeat(30))
-                    wiseSayings.reversed().forEach { // 내림차순
-                        println("${it.id} / ${it.author} / ${it.content}")
-                    }
-                }
-
-                "삭제" -> {
-                    val id = rq.getParamAsInt("id", 0)
-
-                    if (id == 0) {
-                        println("id를 입력해주세요.")
-                        continue
-                    }
-
-                    val target = wiseSayings.firstOrNull {
-                        it.id == id
-                    }
-
-                    if (target == null) {
-                        println("${id}번 명언은 존재하지 않습니다.")
-                        continue
-                    }
-
-                    wiseSayings.remove(target)
-
-                    wiseSayings
-                        .firstOrNull {
-                            it.id == id
-                        }
-                        ?.let {
-                            wiseSayings.remove(it)
-                            println("${id}번 명언이 삭제되었습니다.")
-                        }
-
-                        ?: println("${id}번 명언은 존재하지 않습니다.")
-                    }
-                }
+            when (rq.action) {
+                "종료" -> systemController.exit()
+                "등록" -> wiseSayingController.write()
+                "목록" -> wiseSayingController.list()
+                "삭제" -> wiseSayingController.delete(rq)
             }
         }
     }
+}
