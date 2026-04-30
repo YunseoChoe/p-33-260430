c83d3712e0a73c1233a4e44f793a7d7ceebdc583
choiyunseo <choeyunseo4060@naver.com>
2026-04-30 11:14:05 +0900

    2강 - 등록 기능

--- src/main/kotlin/wiseSaying/Main.kt
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -6,11 +6,18 @@ fun main() {
     while(true) {
         print("명령) ")
 
-        val input = readLine()
+        val input = readln()
         when (input) {
             "종료" -> {
                 println("프로그램을 종료합니다.")
                 break
+            }
+            "등록" -> {
+                print("명언: ")
+                val content = readln()
+
+                print("작가: ")
+                val author = readln()
             }
         }
 
